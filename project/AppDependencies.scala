a2e60c50e6efed5568de3875d699021b55aa23ec
szabolcs-szilagyi-hmrc <131175010+szabolcs-szilagyi-hmrc@users.noreply.github.com>
2023-05-11 14:24:01 +0100

    fix config based on platops bot recommendations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,14 @@
 
 import play.core.PlayVersion.current
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
+  private val bootstrapPlayVersion = "7.1.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.1.0",
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % "7.1.0",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.71.0",
     "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.23.0-play-28",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.3"
@@ -21,6 +23,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"               % current    % "test,it",
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8"   % "test,it",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.71.0"   % "test,it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapPlayVersion   % "test,it",
     "com.github.tomakehurst" % "wiremock-jre8"            % "2.29.1"   % "it"
   )
 
