82760ae6de6af447dae23cee0507a8fd138196c7
szabolcs-szilagyi-hmrc <131175010+szabolcs-szilagyi-hmrc@users.noreply.github.com>
2023-05-15 13:47:53 +0100

    bump up most of the dependencies
    to the latest where it was not breaking the code

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,30 +1,30 @@
-
 import play.core.PlayVersion.current
 import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "7.1.0"
+  private val bootstrapPlayVersion = "7.14.0" // later version breaks the play's binding in this project as of 2023.05 up to version 7.15.0
+  private val mongoVersion = "0.74.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.71.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.23.0-play-28",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.3"
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "6.8.0-play-28", // later version breaks nonce-CSP as of 2023.05 up to version 7.7.0
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.2"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"            % "mockito-core"             % "3.11.2"   % "test",
-    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"    % "test,it",
-    "org.scalatest"          %% "scalatest"               % "3.1.1"    % "test,it",
-    "org.jsoup"              % "jsoup"                    % "1.14.1"   % "test,it",
-    "com.typesafe.play"      %% "play-test"               % current    % "test,it",
-    "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8"   % "test,it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.71.0"   % "test,it",
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapPlayVersion   % "test,it",
-    "com.github.tomakehurst" % "wiremock-jre8"            % "2.29.1"   % "it"
+    "org.mockito"             % "mockito-core"            % "5.2.0"              % "test",
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"           % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"              % "test,it",
+    "org.scalatest"          %% "scalatest"               % "3.2.15"             % "test,it",
+    "org.jsoup"               % "jsoup"                   % "1.15.4"             % "test,it",
+    "com.typesafe.play"      %% "play-test"               % current              % "test,it",
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.0"             % "test,it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVersion         % "test,it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapPlayVersion % "test,it",
+    "com.github.tomakehurst"  % "wiremock"                % "2.27.2"             % "it"
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
