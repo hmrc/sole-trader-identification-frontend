ed8914b81954b6e6d3010f815c785d8606132140
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2025-07-30 13:11:17 +0100

    [VER-5672]Reverted play updates

--- it/test/uk/gov/hmrc/soletraderidentificationfrontend/assets/MessageLookup.scala
+++ it/test/uk/gov/hmrc/soletraderidentificationfrontend/assets/MessageLookup.scala
@@ -31,7 +31,7 @@ object MessageLookup {
     val back = "Back"
     val try_again = "Try again"
 
-    val technicalDifficultiesTitle = "Sorry, there is a problem with the service"
+    val technicalDifficultiesTitle = "Sorry, there is a problem with the service - 500"
     val technicalDifficultiesHeading = "Sorry, there is a problem with the service"
 
     object Error {
